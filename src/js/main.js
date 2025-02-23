aa81b4e2d3dc84f4d18483c7639480b470d40d6c
Alexandru Gatea <alexandru.gatea@gmail.com>
2025-02-23 22:54:09 +0200

    added layout elements

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,23 @@ import '../styles/style.scss';
 // Import html components
 import sideBar from "../components/sidebar.html";
 import statusBar from "../components/statusbar.html";
+import main from "../components/main.html";
+
+const app = document.getElementById('app');
 
 const sidebarContainer = document.getElementById('sideBar');
 sidebarContainer.innerHTML = sideBar;
 
 const statusBarContainer = document.getElementById('statusBar');
 statusBarContainer.innerHTML = statusBar;
+
+const mainContainer = document.getElementById('main');
+mainContainer.innerHTML = main;
+
+sidebarContainer.addEventListener("click", toggleSideBar);
+
+function toggleSideBar() {
+    const sidebarStatus = sidebarContainer.getAttribute("aria-expanded") === "true";
+    sidebarContainer.setAttribute("aria-expanded", !sidebarStatus);
+    app.classList.toggle('collapsed');
+}
